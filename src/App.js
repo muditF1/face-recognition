3ab252f4119015aea9505379f559efbd316c05fb
Mudit Singh <mudit@adobe.com>
2019-11-27 19:11:07 +0530

    Updating

--- src/App.js
+++ src/App.js
@@ -4,10 +4,24 @@ import Navigation from './component/Navigation/Navigation';
 import Logo from './component/Logo/Logo';
 import ImageLinkForm from './component/ImageLinkForm/ImageLinkForm';
 import Rank from './component/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const particlesOption = {
+  particles: {
+    number: {
+      value: 200,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles' params={particlesOption} />
       <Navigation />
       <Logo />
       <Rank />

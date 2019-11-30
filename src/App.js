4ff2b2f716d1cc6dbb87b2b5d0d67504f493fd7a
Mudit Singh <mudit@adobe.com>
2019-11-30 10:09:10 +0530

    Fixes

--- src/App.js
+++ src/App.js
@@ -72,19 +72,6 @@ class App extends Component {
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
       .then(response => {
-        if (response) {
-          fetch('http://localhost:3000/image', {
-            method: 'put',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-              id: this.state.user.id
-            })
-          })
-            .then(response => response.json())
-            .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count }))
-            })
-        }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
       .catch(err => console.log(err));
